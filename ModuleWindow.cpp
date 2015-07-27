e52bc6e903b95240cc4ce09923b2f6fc4dbfbac8
Ricard Pillosu <d0n3val@gmail.com>
2015-07-27 20:56:11 +0200

    Hardcoding window Icon
    New app icon

--- ModuleWindow.cpp
+++ ModuleWindow.cpp
@@ -2,6 +2,35 @@
 #include "Application.h"
 #include "ModuleWindow.h"
 
+static void SetSDLIcon(SDL_Window* window)
+{
+	// this will "paste" the struct my_icon into this function
+#include "icon.c"
+
+	// these masks are needed to tell SDL_CreateRGBSurface(From)
+	// to assume the data it gets is byte-wise RGB(A) data
+	Uint32 rmask, gmask, bmask, amask;
+#if SDL_BYTEORDER == SDL_BIG_ENDIAN
+	int shift = (my_icon.bytes_per_pixel == 3) ? 8 : 0;
+	rmask = 0xff000000 >> shift;
+	gmask = 0x00ff0000 >> shift;
+	bmask = 0x0000ff00 >> shift;
+	amask = 0x000000ff >> shift;
+#else // little endian, like x86
+	rmask = 0x000000ff;
+	gmask = 0x0000ff00;
+	bmask = 0x00ff0000;
+	amask = (my_icon.bytes_per_pixel == 3) ? 0 : 0xff000000;
+#endif
+
+	SDL_Surface* icon = SDL_CreateRGBSurfaceFrom((void*)my_icon.pixel_data, my_icon.width,
+												 my_icon.height, my_icon.bytes_per_pixel * 8, my_icon.bytes_per_pixel*my_icon.width,
+												 rmask, gmask, bmask, amask);
+
+	SDL_SetWindowIcon(window, icon);
+	SDL_FreeSurface(icon);
+}
+
 ModuleWindow::ModuleWindow(Application* app, bool start_enabled) : Module(app, start_enabled)
 {
 	window = NULL;
@@ -62,6 +91,7 @@ bool ModuleWindow::Init()
 		{
 			//Get window surface
 			screen_surface = SDL_GetWindowSurface(window);
+			SetSDLIcon(window);
 		}
 	}
 
@@ -93,3 +123,4 @@ void ModuleWindow::ShowMessageBox(const char* title, const char* text)
 {
 	SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_INFORMATION, title, text, window);
 }
+
